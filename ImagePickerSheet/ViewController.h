130c83b038112b8756e19e048d86972bdf207d8a
MarvinMini <n.demidov@nikitas-mac-mini.local>
2016-03-03 04:12:40 -0800

    Initial Commit

---
+++ ImagePickerSheet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImagePickerSheet
+//
+//  Created by Nikita Demidov on 03/03/16.
+//  Copyright © 2016 Nikita Demidov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
